f396c692a99806a0dd42f196fa21a9c345a016da
SkipCode0 <adrianconfig@gmail.com>
2023-10-11 22:45:53 -0500

    first commit

---
+++ 1_repaso.js
@@ -0,0 +1,32 @@
+console.log('Hola Mundo')
+
+let numero = 10
+
+/*
+function saludar(){
+    console.log('Hola')
+}
+
+const saludar = function() {
+    console.log('Hola')
+}
+
+const saludar = () => {
+    caches.log('hola')
+}
+
+//-------------------------
+function sumar(a, b){
+    return a + b
+}
+
+const sumar = (a,b) => a + b
+
+saludar()
+
+
+for (let i = 0; i < 10; i++){
+    console.log('procesado' + i)
+}
+
+*/
